0735bff002f2171d60509d737678b15a09e17a99
Sheikh Huzaifa Farooqi <155949017+SheikhHuzaifa20@users.noreply.github.com>
2025-12-13 03:46:49 +0500

    done

--- resources/views/admin/layout/app.blade.php
+++ resources/views/admin/layout/app.blade.php
@@ -60,6 +60,7 @@
                 }
             }
         </style>
+        
         @yield('content')
 
         <!--start switcher-->
@@ -136,9 +137,9 @@
     <!--plugins-->
     <script src="{{ asset('assets/js/jquery.min.js') }}"></script>
     <!--plugins-->
-    <script src="{{ asset('assets/plugins/perfect-scrollbar/js/perfect-scrollbar.js') }}"></script>
+    {{-- <script src="{{ asset('assets/plugins/perfect-scrollbar/js/perfect-scrollbar.js') }}"></script> --}}
     <script src="{{ asset('assets/plugins/metismenu/metisMenu.min.js') }}"></script>
-    <script src="{{ asset('assets/plugins/apexchart/apexcharts.min.js') }}"></script>
+    {{-- <script src="{{ asset('assets/plugins/apexchart/apexcharts.min.js') }}"></script> --}}
     <script src="{{ asset('assets/plugins/peity/jquery.peity.min.js') }}"></script>
     <script src="{{ asset('assets/plugins/datatable/js/jquery.dataTables.min.js') }}"></script>
     <script src="{{ asset('assets/plugins/datatable/js/dataTables.bootstrap5.min.js') }}"></script>
@@ -148,7 +149,7 @@
         $(".data-attributes span").peity("donut")
         </script>
     <script src="{{ asset('assets/plugins/simplebar/js/simplebar.min.js') }}"></script>
-    <script src="{{ asset('assets/js/main.js') }}"></script>
+    {{-- <script src="{{ asset('assets/js/main.js') }}"></script> --}}
     <script src="{{ asset('assets/js/index.js') }}"></script>
 
     <script>
